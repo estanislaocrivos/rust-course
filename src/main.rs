86d40c1e4bfc0150e6df85a05d6edc657f9292da
Estanislao Crivos <estanislaocrivos@gmail.com>
2025-07-25 12:08:11 -0300

    Updates.

--- src/main.rs
+++ src/main.rs
@@ -106,8 +106,16 @@ fn main() {
     let array: [u8; 5] = [1, 2, 3, 4, 5]; // You can specify type and size, else it will be inferred
     println!("array[0] = {}", array[0]); // Access array elements with the [] notation
     let array = [6, 7, 8, 9, 10, 11, 12]; // It is not necessary to indicate the type or size
-    const ARRAY_SIZE: usize = 32;
+    const ARRAY_SIZE: usize = 10;
     let array: [u16; ARRAY_SIZE] = [0; ARRAY_SIZE]; // You may initialize all the elements to 0
+
+    /* Debug trait. Every time we print something in the console using {} and then calling the variable to be printed, Rust first checks if the variable to be printed implements the display trait. A trait is an interface which must be implemented by the data type. In this case the Display trait allows a data type to be printed on the console. For example, arrays do not implement the Display trait, so we cannot to this: */
+    // println!("The array is {}", array);
+    /* For cases like this we can use the debug trait, which Rust offers to be able to print data types with little effort. We can invoke this trait by using :? inside the print macro:  */
+    println!("The array is {:?}", array);
+
+    /* dbg! macro. This macro allows to  print debug information by adding line and file to the output string. This avoid having to use print with formatting, etc. Should only use during development and debugging. */
+    dbg!(2 + 2); // The data type must implement the debug trait.
 
     /* Tuples are data types which can contain multiple data types tied together */
     let tuple = (10, 20.5, 'H');
@@ -115,6 +123,20 @@ fn main() {
     println!("tuple[0] = {}", tuple.0); // Access tuple elements with the . operator (first index is 0)
     println!("tuple[1] = {}", tuple.1);
     println!("tuple[2] = {}", tuple.2);
+
+    /* Range type. A range is generally used to iterate over an iterable */
+    let range = 0..11; // Exclusive, goes up to the end - 1
+    for element in range {
+        println!("element = {}", element);
+    }
+    let letters = 'a'..'d';
+    for letter in letters {
+        println!("letter = {}", letter);
+    }
+
+    for element in array {
+        println!("element = {}", element);
+    }
 
     /* ========================================================================================== */
 
