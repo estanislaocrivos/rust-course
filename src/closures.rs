ab5d9493ea5fdcff7c58bad67689964fae26119c
Estanislao Crivos <estanislaocrivos@gmail.com>
2025-09-09 08:56:23 -0300

    Updates.

--- src/closures.rs
+++ src/closures.rs
@@ -1,6 +1,11 @@
 // src/closures.rs
 
 pub fn closures_in_rust() {
+    /* Closures are what lambdas in other languages. These are functions which are anon. (do not have a name tag), can only contain a single expression, and are usually defined inline (single line expression)   */
+
+    /* Closures can access outside variables, which classical functions cannot. */
+
+    /* This is a classical  */
     fn sum(a: i32, b: i32) -> i32 {
         a + b
     }
@@ -9,8 +14,6 @@ pub fn closures_in_rust() {
     let sum_fn = sum;
 
     println!("The sum of 2 + 5 is {}", sum_fn(2, 5));
-
-    /* Closures are what lambdas in other languages. These are functions which are anon. (do not have a name tag), can only contain a single expression, and are usually defined inline (single line expression)   */
 
     let sum = |num1: i32, num2: i32| -> i32 { num1 + num2 };
 
--- src/traits.rs
+++ src/traits.rs
@@ -1,15 +1,16 @@
 // src/traits.rs
 
 pub fn traits_in_rust() {
-    struct Human;
-    struct Cat;
+    /* Traits allow to define interfaces for objects, these interfaces may or may not contain an implementation. It allows for a contract to be stablished for certain functionality without the need of implementation. */
 
-    /* A trait can be used to define an interface */
+    /* You can define a trait using the trait directive. Inside the trait block you can declare functions/methods signatures. These signatures may contain self as a parameter or not, depending on the context (method or associated function or a simple function) */
     trait Talk {
         fn say_hello(&self) -> String;
     }
 
-    /* Then you can implement the interface for different objects */
+    /* Then you can implement the interface for different objects: */
+    struct Human;
+    struct Cat;
 
     impl Talk for Human {
         fn say_hello(&self) -> String {
@@ -24,8 +25,33 @@ pub fn traits_in_rust() {
     }
 
     let john = Human;
-    println!("John says {}", john.say_hello());
+    println!("John says: {}", john.say_hello());
 
     let chichi = Cat;
-    println!("Chichi says {}", chichi.say_hello());
+    println!("Chichi says: {}", chichi.say_hello());
+
+    /* You may also define a default implementation for a trait. Is there is no implementation for the trait, this default is used */
+    trait Jump {
+        fn jump(&self) -> String {
+            return "I am jumping!".to_string();
+        }
+    }
+
+    impl Jump for Cat {}
+    impl Jump for Human {}
+
+    println!("John says: {}", john.jump());
+    println!("Chichi says: {}", chichi.jump());
+
+    impl Cat {
+        fn say_something(&self) {
+            println!("{}, miau miau miau miau...", self.say_hello()); // You may also call traits from inside the methods
+        }
+    }
+
+    /* You can pass objects which implement certain trait as parameter, for example */
+    fn take_one(entity: &impl Talk) {
+        println!("The entity says: {}", entity.say_hello());
+    }
+    take_one(&chichi);
 }
